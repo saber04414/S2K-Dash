2081a66b2b7a44b8505ba43c2b88645789445e48
0xsid0703 <daniel73matthew1213@gmail.com>
2025-04-14 13:08:47 -0400

    added

--- app/api/getRegistrationCost/route.ts
+++ app/api/getRegistrationCost/route.ts
@@ -2,12 +2,16 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 import axios from 'axios';
+import prisma from '@/lib/prisma';
 export async function GET() {
     const data = []
     const subnets_res = await axios.get('https://taomarketcap.com/api/subnets')
     const subnets = await subnets_res.data
-    console.log("Length: ", subnets.length)
-    const subnets_slice = subnets.slice(50, subnets.length)
+    const subnets_ids = await prisma.subnets.findMany({ orderBy: { subnet: 'asc' } });
+    const mysubnets = subnets_ids.map((ss: any) => ss.subnet);
+    const arr = Array.from({ length: subnets.length - 50 }, (_, i) => i + 50);
+    const merged = Array.from(new Set([...arr, ...mysubnets])).sort((a, b) => a - b);
+    const subnets_slice = subnets.filter((item:any)=> merged.includes(item.subnet))
     try {
         for(const subnet of subnets_slice) {
             const response = await axios.post(`https://taomarketcap.com/api/subnets/${subnet.subnet}/burn`)
