2c425e93ac88898a913b9a5541ea087b8bdb5a35
0xsid0703 <daniel73matthew1213@gmail.com>
2025-05-02 14:01:21 -0400

    added

--- app/(auth)/my-status/[subnet]/page.tsx
+++ app/(auth)/my-status/[subnet]/page.tsx
@@ -5,6 +5,7 @@ import { fetcher } from '@/utils/fetcher'
 import { showKey, copyKey, showNumber, showTaoNumber } from '@/lib/main'
 import { Immune, Active, Danger } from '@/components/MinerIcon'
 import ImageLoadingSpinner from '@/components/ImageLoadingSpinner'
+import { ArrowLeft } from 'lucide-react'
 import {
     Tooltip,
     TooltipContent,
@@ -12,8 +13,10 @@ import {
     TooltipTrigger,
 } from "@/components/ui/tooltip"
 import { useParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 const MyStatusPage = () => {
+    const router = useRouter()
     const params = useParams();
     const { data, error, isLoading } = useSWR(`/api/myStatus?subnet=${params.subnet}`, fetcher, {
         refreshInterval: 12_000,
@@ -56,46 +59,46 @@ const MyStatusPage = () => {
         Data Fetching Error
     </div>
     if (data) {
-        console.log(data)
         return (
-            <div className='w-full flex flex-col gap-5 justify-center'>
+            <div className='w-full flex flex-col gap-10 justify-center relative'>
+                <div className='absolute top-0 left-0 w-fit text-center p-2 cursor-pointer flex flex-row gap-2 items-center' onClick={() => router.back()}><ArrowLeft size={18}/>Back</div>
                 <div className='text-2xl font-bold text-center'>My Status</div>
                 <div className='flex flex-col gap-10'>
                     {
                         data && data.data && data.bittensor_data && (
                             <div className='flex flex-col gap-1'>
-                            <div className='flex flex-row justify-between items-end'>
-                                <TooltipProvider>
-                                    <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <div className='text-xl text-semibold text-left underline cursor-pointer'>Subnet {data.data.subnet}</div>
-                                        </TooltipTrigger>
-                                        <TooltipContent side="bottom" className='border-slate-500 border'>
-                                            <div className='flex flex-col gap-2'>
-                                                <div className='text-base font-bold'>Subnet Information</div>
-                                                <div className='flex flex-col gap-0'>
-                                                    <div className='text-sm'>Name: {data.data.name} {data.data.letter}</div>
-                                                    <div className='text-sm'>Emission: {showNumber(data.data.emission, 4)} %</div>
-                                                    <div className='text-sm'>Tao In Pool: {showTaoNumber(data.data.taoInpool)} 𝞃</div>
-                                                    <div className='text-sm'>Alpha In Pool: {showTaoNumber(data.data.alphaInpool)} 𝞃</div>
+                                <div className='flex flex-row justify-between items-end'>
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <div className='text-xl text-semibold text-left underline cursor-pointer'>Subnet {data.data.subnet}</div>
+                                            </TooltipTrigger>
+                                            <TooltipContent side="bottom" className='border-slate-500 border'>
+                                                <div className='flex flex-col gap-2'>
+                                                    <div className='text-base font-bold'>Subnet Information</div>
+                                                    <div className='flex flex-col gap-0'>
+                                                        <div className='text-sm'>Name: {data.data.name} {data.data.letter}</div>
+                                                        <div className='text-sm'>Emission: {showNumber(data.data.emission, 4)} %</div>
+                                                        <div className='text-sm'>Tao In Pool: {showTaoNumber(data.data.taoInpool)} 𝞃</div>
+                                                        <div className='text-sm'>Alpha In Pool: {showTaoNumber(data.data.alphaInpool)} 𝞃</div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                                <div className='flex flex-row gap-5 items-center'>
-                                    <div className='text-sm pr-5'>Alpha: {showNumber(data.data.price, 4)} 𝞃 / {showNumber(data.data.price * data.taoPrice, 4)} $</div>
-                                    <div className='text-sm pr-5'>Registration: {showNumber(data.data.regcost, 4)} 𝞃</div>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                    <div className='flex flex-row gap-5 items-center'>
+                                        <div className='text-sm pr-5'>Alpha: {showNumber(data.data.price, 4)} 𝞃 / {showNumber(data.data.price * data.taoPrice, 4)} $</div>
+                                        <div className='text-sm pr-5'>Registration: {showNumber(data.data.regcost, 4)} 𝞃</div>
 
-                                    <div className='text-sm pr-5'>Mechanism: {incentive_countdowns === -1
-                                        ? '-1'
-                                        : `${Math.floor(incentive_countdowns / 3600)}h ${Math.floor((incentive_countdowns % 3600) / 60)}m ${incentive_countdowns % 60}s`
-                                    }</div>
-                                    <div className='text-sm pr-5'>Registration: {registration_countdowns === -1
-                                        ? '-1'
-                                        : `${Math.floor(registration_countdowns / 3600)}h ${Math.floor((registration_countdowns % 3600) / 60)}m ${registration_countdowns % 60}s`
-                                    }</div>
-                                </div>
+                                        <div className='text-sm pr-5'>Mechanism: {incentive_countdowns === -1
+                                            ? '-1'
+                                            : `${Math.floor(incentive_countdowns / 3600)}h ${Math.floor((incentive_countdowns % 3600) / 60)}m ${incentive_countdowns % 60}s`
+                                        }</div>
+                                        <div className='text-sm pr-5'>Registration: {registration_countdowns === -1
+                                            ? '-1'
+                                            : `${Math.floor(registration_countdowns / 3600)}h ${Math.floor((registration_countdowns % 3600) / 60)}m ${registration_countdowns % 60}s`
+                                        }</div>
+                                    </div>
                                 </div>
                                 <table className='w-full'>
                                     <thead>
