7b607e037e8bd2e5c8f4cccdd642ae5c2af1d4ac
0xsid0703 <daniel73matthew1213@gmail.com>
2025-05-04 23:39:40 -0400

    added

--- components/SubnetItem.tsx
+++ components/SubnetItem.tsx
@@ -26,28 +26,28 @@ type Props = {
 
 const SubnetItem = (props: Props) => {
     const router = useRouter()
-    const [src, setSrc] = useState(props.image_url || '/default.png')
+    const [src, setSrc] = useState(props.image_url || './default.png')
     return (
-        <div className='w-full rounded-md border border-slate-500 cursor-pointer flex flex-col gap-0' onClick={() => router.push(`/my-status/${props.subnet_id}`)}>
-            <Image className='w-full h-56 bg-slate-500 rounded-t-md' src={src} width="500" height="500" alt='' onError={() => setSrc('/fallback.jpg')} />
-            <div className='flex flex-row justify-between items-center px-4 py-2'>
-                <div className='text-xl font-bold'>Subnet {props.subnet_id}</div>
-                <div className='text-sm'>UIDs: {props.active} / {props.maxAllowedUids}</div>
+        <div className='w-full rounded-md border border-slate-500 cursor-pointer flex flex-col gap-0 hover:scale-[1.01] transition-all relative' onClick={() => router.push(`/my-status/${props.subnet_id}`)}>
+            <div className="absolute inset-0 bg-gray-200 opacity-0 hover:opacity-40 transition-opacity duration-300" />
+            <Image className='w-full h-56 bg-slate-500 rounded-t-md' src={src} width="500" height="500" alt='' onError={() => setSrc('./default.png')} />
+            <div className='py-2 flex flex-col gap-1'>
+                <div className='flex flex-row justify-between items-center px-4 pb-2'>
+                    <div className='text-xl font-bold'>Subnet {props.subnet_id}</div>
+                    <div className='text-sm'>UIDs: {props.active} / {props.maxAllowedUids}</div>
+                </div>
+                <div className='flex flex-row justify-between items-center px-4'>
+                    <div className='text-sm'>Name: {props.name}</div>
+                    <div className='text-sm'>Emission: {showNumber(props.emissionRate, 4)} %</div>
+                </div>
+                <div className='flex flex-row justify-between items-center px-4'>
+                    <div className='text-sm'>Alpha In Pool: {showTaoNumber(props.subnetAlphaIn)} 𝞃</div>
+                    <div className='text-sm'>TAO In Pool: {showTaoNumber(props.subnetTAO)} 𝞃</div>
+                </div>
+                {props.github && <div className='flex justify-end px-4 pt-2 z-50'>
+                    <button className='hover:scale-[1.2] transition-all' onClick={() => router.push(props.github)}><GithubIcon size={18} /></button>
+                </div>}
             </div>
-            <div className='px-4'>
-                <div className='text-sm'>Description: {props.description}</div>
-            </div>
-            <div className='flex flex-row justify-between items-center px-4'>
-                <div className='text-sm'>Name: {props.name}</div>
-                <div className='text-sm'>Emission: {showNumber(props.emission, 4)} %</div>
-            </div>
-            <div className='flex flex-row justify-between items-center px-4'>
-                <div className='text-sm'>Alpha In Pool: {showTaoNumber(props.subnetAlphaIn)}</div>
-                <div className='text-sm'>TAO In Pool: {showTaoNumber(props.subnetTAO)}</div>
-            </div>
-            {props.github && <div className='flex flex-end px-4'>
-                <button onClick={() => router.push(props.github)}><GithubIcon size={18} /></button>
-            </div>}
         </div>
     )
 }
