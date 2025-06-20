bcd20c47860eb12e03d9d64ea399694537455d62
unknown <daniel73matthew1213@gmail.com>
2025-06-20 13:50:12 -0700

    added

--- components/MinersChart.tsx
+++ components/MinersChart.tsx
@@ -28,7 +28,7 @@ const CustomizedBar = (props: any) => {
       const match =
         item.coldkey?.toLowerCase().includes(searchLower) ||
         item.hotkey?.toLowerCase().includes(searchLower);
-      fillColor = match ? "#F90" : "#d1d5db"; // Highlight or gray
+      fillColor = match ? "#ccc" : "#666666"; // Highlight or gray
     } else {
       // Original logic
       fillColor =
