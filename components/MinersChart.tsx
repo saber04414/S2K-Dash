f22e2a3651442e7ef227792c5f8d34cc7fdd20f0
unknown <daniel73matthew1213@gmail.com>
2025-06-20 13:45:51 -0700

    added

--- components/MinersChart.tsx
+++ components/MinersChart.tsx
@@ -1,5 +1,6 @@
+"use client"
 import { formattedTimeFromSeconds } from "@/utils/date";
-import React from "react";
+import React, { useState } from "react";
 import {
   Bar,
   BarChart,
@@ -13,11 +14,33 @@ import {
 import { showStatusKey } from "@/lib/main";
 
 const CustomizedBar = (props: any) => {
-  const { formattedGraphicalItems } = props;
+  const { formattedGraphicalItems, search } = props;
   if (!formattedGraphicalItems || !formattedGraphicalItems[0]?.props?.data)
     return null;
+
   const firstSeries = formattedGraphicalItems[0];
+  const searchLower = search.toLowerCase();
+
   return firstSeries?.props?.data?.map((item: any) => {
+    let fillColor: string;
+
+    if (search !== "") {
+      const match =
+        item.coldkey?.toLowerCase().includes(searchLower) ||
+        item.hotkey?.toLowerCase().includes(searchLower);
+      fillColor = match ? "#F90" : "#d1d5db"; // Highlight or gray
+    } else {
+      // Original logic
+      fillColor =
+        item.owner === "Mine"
+          ? "#f34"
+          : item.owner === "Unknown"
+          ? item.immunity === true
+            ? "#F90"
+            : "#00DBBC"
+          : "#ccc";
+    }
+
     return (
       <Rectangle
         key={item.uid}
@@ -25,15 +48,7 @@ const CustomizedBar = (props: any) => {
         height={5}
         x={item.x}
         y={item.y - 5}
-        fill={
-          item.owner === "Mine"
-            ? "#f34"
-            : item.owner === "Unknown"
-            ? item.immunity === true
-              ? "#F90"
-              : "#00DBBC"
-            : "#ccc"
-        }
+        fill={fillColor}
       />
     );
   });
@@ -67,12 +82,16 @@ const CustomTooltip = (props: any) => {
 };
 
 const MinersChart = ({ chartData }: any) => {
+  const [search, setSearch] = useState("");
+
   const data = chartData;
   if (!data || data.length === 0) {
     return <div>No data available</div>;
   }
+
   return (
     <div className="relative w-full h-[300px]">
+      <input type="text" placeholder="Search..." className="px-2 py-1 text-sm text-white bg-slate-900 border border-slate-500 focus:border-slate-200 absolute z-10 right-0 top-5 rounded-md" onChange={(e) => setSearch(e.target.value)} />
       <ResponsiveContainer width="100%" height="100%">
         <BarChart
           barCategoryGap={0.4}
@@ -96,7 +115,7 @@ const MinersChart = ({ chartData }: any) => {
           <YAxis stroke="#fff2" fontSize={12} type="number" />
           <Tooltip cursor={{ fill: "#29f2" }} content={<CustomTooltip />} />
           <Bar dataKey="daily" fill="#0000" isAnimationActive={false} />
-          <Customized component={CustomizedBar} />
+          <Customized component={<CustomizedBar search={search} />} />
         </BarChart>
       </ResponsiveContainer>
     </div>
