0c2dc2bd51566395ec691c16308530b7d12fbe68
saber04414 <dev.coder2400@gmail.com>
2026-01-20 20:08:40 -0800

    added

--- app/bid/page.tsx
+++ app/bid/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import useSWR, { mutate } from 'swr'
