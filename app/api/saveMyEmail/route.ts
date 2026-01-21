b760032a22a243aff2e59e4fb627e1c08afd5753
saber04414 <dev.coder2400@gmail.com>
2026-01-20 20:15:53 -0800

    updated

--- app/api/saveMyEmail/route.ts
+++ app/api/saveMyEmail/route.ts
@@ -2,20 +2,21 @@ import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-    const { name, email } = await req.json();
+    const { email, secretPassword } = await req.json();
     // Validate input
-    if (!name || typeof name !== 'string' || !email || typeof email !== 'string') {
+    if (!email || typeof email !== 'string' || !secretPassword || typeof secretPassword !== 'string') {
         return NextResponse.json({ error: 'Invalid data value' }, { status: 400 });
     }
 
     try {
         const savedEmail = await prisma.myEmail.create({
             data: { 
-                email, 
+                email,
+                secretPassword,
             },
         });
 
-        return NextResponse.json({ email: savedEmail }, { status: 200 });
+        return NextResponse.json({ myemail: savedEmail }, { status: 200 });
     } catch (error) {
         console.error('Error saving email:', error);
         return NextResponse.json({ error: 'Failed to save email' }, { status: 500 });
